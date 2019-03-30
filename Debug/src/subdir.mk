5c2913c5c78c7569d47e87f8360009e845ca7e75
ivliev123 <123ivliev123@mail.ru>
2019-03-30 20:47:18 +0300

    --all

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c \
+../src/stm32f1xx_hal_msp.c \
+../src/stm32f1xx_it.c \
+../src/syscalls.c \
+../src/system_stm32f1xx.c 
+
+OBJS += \
+./src/main.o \
+./src/stm32f1xx_hal_msp.o \
+./src/stm32f1xx_it.o \
+./src/syscalls.o \
+./src/system_stm32f1xx.o 
+
+C_DEPS += \
+./src/main.d \
+./src/stm32f1xx_hal_msp.d \
+./src/stm32f1xx_it.d \
+./src/syscalls.d \
+./src/system_stm32f1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -DSTM32F103xB -DUSE_HAL_DRIVER -I"C:/Users/123iv/workspace/test_led2/HAL_Driver/Inc/Legacy" -I"C:/Users/123iv/workspace/test_led2/inc" -I"C:/Users/123iv/workspace/test_led2/CMSIS/device" -I"C:/Users/123iv/workspace/test_led2/CMSIS/core" -I"C:/Users/123iv/workspace/test_led2/HAL_Driver/Inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
